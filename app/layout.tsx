43975a6896218feefa5e49a72fa1707026ba6d55
Raghvendra9402 <singhraghvendra405@gmail.com>
2025-01-09 16:34:59 +0530

    "update"

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
-import icon from "@/public/Designer.png";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
